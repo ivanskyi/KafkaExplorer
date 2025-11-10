46ef500c6cbe3aaf2c0f3d0701a108bd8748ba00
ivanskyi <olehivanskyi@gmail.com>
2025-11-10 20:49:14 +0200

    Created project

---
+++ src/main/java/com/ivanskyi/kafka/KafkaExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.ivanskyi.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaExplorerApplication.class, args);
+    }
+
+}
